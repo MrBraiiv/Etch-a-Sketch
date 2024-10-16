4220785cfdf1c5bcc23b42a5f9bb825ebeaad4a7
Mr braiiv <mikam6285@gmail.com>
2024-10-16 18:01:48 +0300

    deleted few lines

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// function sketch() {
-//   const column = Array(16)
-//     .fill()
-//     .map((e) => []);
-
-//   for (i = 0; i < 16; i++) {
-//     column[i].concat(row(document.createElement("div")));
-//   }
-
-//   return column;
-
-//   function row(square) {
-//     return Array(16)
-//       .fill()
-//       .map((x) => [square]);
-//   }
-// }
-
 const container = document.querySelector(".container");
 for (let i = 1; i < 16 * 16; i++) {
   const square = document.createElement("div");
