f1fa006e8f20950bb7ae3f3b1b4df2a3bc55cf91
Mr braiiv <mikam6285@gmail.com>
2024-10-16 14:26:51 +0300

    Add a pixelated tail event that follows the mouse.

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ for (let i = 1; i < 16 * 16; i++) {
   const square = document.createElement("div");
   container.appendChild(square);
 }
+
+//Change color by hovering
+container.addEventListener("mousemove", (e) => {
+  if (e.target !== container) {
+    e.target.classList.add("fired-square");
+  }
+});
+
+//Mouse trail
+container.addEventListener("mousemove", (e) => {
+  if (e.target !== container) {
+    const x = e.pageX;
+    const y = e.pageY;
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dot.style.left = `${x - 10 + "px"}`;
+    dot.style.top = `${y - 10 + "px"}`;
+    setTimeout(() => {
+      container.appendChild(dot);
+    }, 50);
+  }
+});
